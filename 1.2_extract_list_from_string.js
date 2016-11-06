50a07b59414d9f2be24ed8b8c822a9986eef9ca8
bugzhang <sakura@csu.edu.cn>
2016-11-06 10:03:59 +0800

    no message

--- 1.2_extract_list_from_string.js
+++ 1.2_extract_list_from_string.js
@@ -1,3 +1,15 @@
 /**
  * Created by bugzhang on 2016/11/5.
  */
+//待提取字符串
+var s = "this is to do list:coding,eating,playing,sleeping.";
+//找到冒号
+var left = s.indexOf(':');
+//找到句号
+var right = s.indexOf('.',left+1);
+//提取后字符串
+var listStr = s.substring(left+1,right);
+//字符串转数组
+var list = listStr.split(',');
+//输出数组
+console.log(list);
---
+++ 1.3_typeof.js
@@ -0,0 +1,20 @@
+/**
+ * Created by bugzhang on 2016/11/6.
+ */
+//定义变量
+var num = 12;
+var str = 'string';
+var bool = true;
+var fun = function () {
+    return 0;
+};
+var a;
+var arr = ['a','r','r'];
+//测试用例
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(fun));
+console.log(typeof(a));
+console.log(typeof(arr));
+console.log(typeof(b));
