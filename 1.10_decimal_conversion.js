e84dfba72c86843422794ba35fcd2cfd83413d73
bugzhang <sakura@csu.edu.cn>
2016-11-07 17:48:47 +0800

    添加1.13

---
+++ 1.10_decimal_conversion.js
@@ -0,0 +1,7 @@
+/**
+ * Created by bugzhang on 2016/11/6.
+ */
+var num = 123;
+console.log("二进制是："+num.toString(2));
+console.log("十进制是："+num.toString(10));
+console.log("十六进制是："+num.toString(16));
